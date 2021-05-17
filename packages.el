5b3456993ba6c9139f3a1c078168c45d577c475c
Kevin Garner <kevin@kgar.net>
2021-05-16 19:51:06 -0500

    added 2 packages

--- packages.el
+++ packages.el
@@ -12,6 +12,8 @@
 (package! rust-mode)
 (package! company-c-headers)
 (package! all-the-icons)
+(package! org-bullets)
+;;(package! dired-open)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
